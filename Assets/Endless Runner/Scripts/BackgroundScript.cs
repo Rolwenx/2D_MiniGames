649e765c6a8b590b981a604307db42fa2d16c264
Rolwen <n.rnolwen@gmail.com>
2025-05-08 21:35:49 +0200

    ER Game - Made Parallax background go fast too

--- Assets/Endless Runner/Scripts/BackgroundScript.cs
+++ Assets/Endless Runner/Scripts/BackgroundScript.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class BackgroundScript : MonoBehaviour
 {
    
-   [SerializeField] private float _speed;
-   [SerializeField] private Renderer _bgRend;
+   [SerializeField] private float _initialSpeed = 0.1f;
+    [SerializeField] private float _maxSpeed = 1f;
+    [SerializeField] private float _acceleration = 0.01f;
+    [SerializeField] private Renderer _bgRend;
 
-   void Update(){
-        _bgRend.material.mainTextureOffset += new Vector2(_speed*Time.deltaTime,0);
-        
-   }
+    private float _currentSpeed;
+
+    void Start()
+    {
+        _currentSpeed = _initialSpeed;
+    }
+
+    void Update()
+    {
+        _currentSpeed = Mathf.Min(_currentSpeed + _acceleration * Time.deltaTime, _maxSpeed);
+        _bgRend.material.mainTextureOffset += new Vector2(_currentSpeed * Time.deltaTime, 0);
+    }
 }
